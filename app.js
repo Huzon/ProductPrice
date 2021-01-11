7ae8a2731f277103052adcf9637d4c2010ba5a71
Huzefa Mohammed Hussain <huzefamhd@gmail.com>
2021-01-11 17:54:20 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+const { render } = require("ejs");
+const app = express();
+
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "productprices"
+});
+
+// var brands = [];
+
+con.connect();
+
+
+app.get("/", (req, res) => {
+    con.query("SELECT * FROM brand", (err, rows, fields) => {
+        if (err)
+            console.log(err);
+
+        else if (rows.length > 0) {
+            console.log("has vaues");
+            var brands = rows;
+        }
+        var products
+        brands.forEach(brand => {
+            console.log(`this is ${brand.name}`);
+            con.query(`SELECT * FROM prodprice where brandID =${brand.brandID}`, (err, rows, fields) => {
+                if (err)
+                    console.log(err);
+                else if (rows.length > 0) {
+
+                    // console.log(brand.name + " " + rows[0].name);
+                    // console.log("has vaues");
+                }
+            });
+        });
+
+    });
+});
+// 
+// app.post("/brandForm", (req, res) => {
+//     console.log(req.body.);
+// });
+
+
+
+
+// app.get("/", (req, res) => {
+
+// });
+
+
+
+
+app.listen("3000", () => {
+    console.log("Connected to server 3000");
+});
---
+++ public/JS/main.js
@@ -0,0 +1,20 @@
+// document.querySelector(".toggle-button").click(function() {
+//     $(this).parent().find("ul").slideToggle(function() {
+//         // Animation complete.
+//     });
+// });
+
+// document.querySelector(".brand").click(function() {
+//     var id = this.id;
+//     console.log(id);
+// });
+function brandClicked(clickID) {
+    alert(clickID);
+
+}
+// function brandClicked(event) {
+//     if (event.target.tagName == "a") {
+//         console.log("btnClicked");
+//         alert(event.target.id);
+//     }
+// }
